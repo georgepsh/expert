ef3cf274311de82d15319562fc57baeaa56472c0
georgepsh <georgii39@gmail.com>
2021-09-19 23:32:31 +0300

    tf fix

--- baselines/acktr/acktr_disc.py
+++ baselines/acktr/acktr_disc.py
@@ -1,8 +1,8 @@
 import joblib
 import numpy as np
 import tensorflow as tf
-from tensorflow.compat.v1 import ConfigProto
-from tensorflow.compat.v1 import InteractiveSession
+from tensorflow import ConfigProto
+from tensorflow import InteractiveSession
 
 config = ConfigProto()
 config.gpu_options.allow_growth = True
